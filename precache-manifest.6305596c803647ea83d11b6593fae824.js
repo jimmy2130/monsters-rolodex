14948637d846cc2cf4af1fbf3d23c98738133f94
陳鎮宇 <ccking37@gmail.com>
2020-03-26 00:22:45 +0800

    Updates

---
+++ precache-manifest.6305596c803647ea83d11b6593fae824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7023c75f27a47aff3b3776bb56db2ba",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "24f9038a6c2b8dbe5bd5",
+    "url": "/monsters-rolodex/static/css/main.3de3dfa9.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f9038a6c2b8dbe5bd5",
+    "url": "/monsters-rolodex/static/js/main.2c95ce77.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
